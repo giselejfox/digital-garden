99bb678ffaf14b1dea83050f3b730a9b7425875b
giselejfox <giselejfox@users.noreply.github.com>
2025-12-03 06:07:21 +0000

    deploy: 290a7f30905109a177d0fc1dff96b7060a304c28

--- _next/static/oBDzWsp41iK28t9x3JNQN/_buildManifest.js
+++ _next/static/oBDzWsp41iK28t9x3JNQN/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/digital-garden//_next/:path+",
+        "destination": "/digital-garden/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
