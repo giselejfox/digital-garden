d3d7cc3f0fe4efe9b9671a82d15baafdc70c1f69
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2025-12-02 20:17:29 -0800

    make Next.js react app and get basic format

---
+++ app/cd-shelf/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="container py-5 mt-3">
+      <main>
+        <h1 className="text-5xl font-bold text-zinc-900 dark:text-white sm:text-6xl">
+          Welcome to dkwjfscsoiucoineiovncoirnvio<a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,43 @@
+export default function Home() {
+  return (
+    <div className="container py-5 mt-3">
+      <main>
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-6 ">
+            <h1>🌱 Welcome to Gisele&apos;s Garden</h1>
+            <p>This is a site that&apos;s a bit less profesh and a bit more where I can ramble and share myself online in a more creative way.</p>
+            <p>Welcome in!</p>
+
+            {/* COLLECTIONS */}
+            <h2 className="about-h2">collections</h2>
+            <div className="horizontal-line mb-3"></div>
+            <ul>
+              <li><a href='/cd-shelf'>cd shelf</a> - where I show you all the CDs I've listened to in totality</li>
+              <li><a href='/cd-shelf'>book shelf</a> - where I talk about my favorite books</li>
+              <li><a href='/cd-shelf'>my favorite clothes</a> - where I brag about my cool clothing</li>
+              <li><a href='/cd-shelf'>my favorite websites</a> - where I entice you to click on links</li>
+            </ul>
+
+            {/* CREATIONS */}
+            <h2 className="about-h2">creations</h2>
+            <div className="horizontal-line mb-3"></div>
+            <ul>
+              <li><a href='/cd-shelf'>film photo gallery</a></li>
+              <li><a href='/cd-shelf'>fiber arts gallery</a></li>
+              <li><a href='/cd-shelf'>coding experiments</a></li>
+            </ul>
+
+            {/* WRITINGS */}
+            <h2 className="about-h2">writings</h2>
+            <div className="horizontal-line mb-3"></div>
+            <ul>
+              <li><a href='/cd-shelf'>series of blog posts from my study abroad in Amsterdam</a></li>
+              <li><a href='/cd-shelf'>maybe a blog lives here...</a></li>
+            </ul>
+
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
